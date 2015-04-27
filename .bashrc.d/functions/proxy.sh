af16b09a8fa45525ae4d72a53238af45e3966938
rendo0m <andi.arbait@gmail.com>
2015-04-27 23:19:33 +0200

    corrected proxy.sh

--- .bashrc.d/functions/proxy.sh
+++ .bashrc.d/functions/proxy.sh
@@ -2,16 +2,16 @@
 # Beware doing too much hardcoing in case giving VM to s'one else!
 
 # Enable Proxy
-function proxy(){
+function proxy() {
 	echo -e
 	echo -n "Username : "
 	read -e username
 		if ! which cntlm > /dev/null; then
 			echo -n "Password : "
 			read -es password
-			#wanna kick next two lines and hardcode your $proxyaddr ?
-            echo -n "Proxy (host:port) : "
-            read -e proxyaddr
+            #wanna kick next two lines and hardcode your $proxyaddr ?
+			echo -n "Proxy (host:port) : "
+                        read -e proxyaddr
 			export http_proxy=http://$username:$password@$proxyaddr/
 			export https_proxy=$http_proxy
 			export ftp_proxy=$http_proxy
@@ -34,48 +34,48 @@ function proxy(){
             if [ ! -f "/etc/cntlm.conf" ]; then
                 echo -e "\n File \"/etc/cntlm.conf\"not found!\n"; 
             else
-
-		echo -n "Domain	: "
-		read -e domain
-		echo -n "Proxy (host:port) : "
-		read -e proxyaddr
-		sudo sh -c "/etc/init.d/cntlm stop"
-		sudo sh -c "sed -i 's/Username.*/Username\t$username/g' /etc/cntlm.conf"
-		sudo sh -c "sed -i 's/Domain.*/Domain\t\t$domain/g' /etc/cntlm.conf"
-		sudo sh -c "sed -i 's/Proxy.*/Proxy\t\t$proxyaddr/g' /etc/cntlm.conf"
-		sudo sh -c "cntlm -v -I -M http://www.heise.de -c /etc/cntlm.conf"
-		echo -e ================================================================================
-		echo -e
-		echo -e "\033[31mJUST FILL IN THE HASH FROM ABOVE\033[0m"
-		echo -e "(if there is none, wrong user/pass?)"
-		echo -e
-		echo -e	"Auth        NTLMv2"
-		echo -n "PassNTLMv2: "
-                read -e password
-		sudo sh -c "sed -i 's/PassNTLMv2.*/PassNTLMv2\t$password/g' /etc/cntlm.conf"
-		sudo sh -c "/etc/init.d/cntlm start"
-		export http_proxy='http://localhost:3128'
-		export https_proxy=$http_proxy
-		export ftp_proxy=$http_proxy
-		export rsync_proxy=$http_proxy
-		export no_proxy="localhost,127.0.0.1"
-		if which git > /dev/null; then
-			git config --global http.proxy $http_proxy
-			git config --global https.proxy $http_proxy
-			git config --global git.proxy $http_proxy
-		fi
-		if which curl > /dev/null; then
-			alias curl="curl -x $http_proxy"
-		fi
-		echo -e "\033[31m"
-		sudo sh -c '(echo "Acquire::http::proxy \"'$http_proxy'\";"; echo "Acquire::ftp::proxy \"'$http_proxy'\";"; echo "Acquire::https::proxy \"'$http_proxy'\";") > /etc/apt/apt.conf'
-		echo -e "\033[32mProxy environment variable set.\033[0m"
-		echo -e
-		fi
+            echo -n "Domain	: "
+            read -e domain
+            echo -n "Proxy (host:port) : "
+            read -e proxyaddr
+            sudo sh -c "/etc/init.d/cntlm stop"
+            sudo sh -c "sed -i 's/Username.*/Username\t$username/g' /etc/cntlm.conf"
+            sudo sh -c "sed -i 's/Domain.*/Domain\t\t$domain/g' /etc/cntlm.conf"
+            sudo sh -c "sed -i 's/Proxy.*/Proxy\t\t$proxyaddr/g' /etc/cntlm.conf"
+            sudo sh -c "cntlm -v -I -M http://www.heise.de -c /etc/cntlm.conf"
+            echo -e ================================================================================
+            echo -e
+            echo -e "\033[31mJUST FILL IN THE HASH FROM ABOVE\033[0m"
+            echo -e "(if there is none, wrong user/pass?)"
+            echo -e
+            echo -e	"Auth        NTLMv2"
+            echo -n "PassNTLMv2: "
+                    read -e password
+            sudo sh -c "sed -i 's/PassNTLMv2.*/PassNTLMv2\t$password/g' /etc/cntlm.conf"
+            sudo sh -c "/etc/init.d/cntlm start"
+            export http_proxy='http://localhost:3128'
+            export https_proxy=$http_proxy
+            export ftp_proxy=$http_proxy
+            export rsync_proxy=$http_proxy
+            export no_proxy="localhost,127.0.0.1"
+            if which git > /dev/null; then
+                git config --global http.proxy $http_proxy
+                git config --global https.proxy $http_proxy
+                git config --global git.proxy $http_proxy
+            fi
+            if which curl > /dev/null; then
+                alias curl="curl -x $http_proxy"
+            fi
+            echo -e "\033[31m"
+            sudo sh -c '(echo "Acquire::http::proxy \"'$http_proxy'\";"; echo "Acquire::ftp::proxy \"'$http_proxy'\";"; echo "Acquire::https::proxy \"'$http_proxy'\";") > /etc/apt/apt.conf'
+            echo -e "\033[32mProxy environment variable set.\033[0m"
+            echo -e
+            fi
+        fi
 }
 
 # Disable Proxy
-function proxyoff(){
+function proxyoff() {
 		unset http_proxy
 		unset https_proxy
 		unset ftp_proxy
@@ -102,7 +102,7 @@ function proxyoff(){
 		echo -e
 }
 
-#Sometime NTP not reachable through proxy, so ~/>synctime
-function synctime(){
-    sudo date -s "$(curl -sD - google.com | grep ^Date: | cut -d' ' -f3-6)Z"
+# Sometimes sync fails through proxy
+function synctime() {
+     sudo date -s "$(curl -sD - google.com | grep ^Date: | cut -d' ' -f3-6)Z"
 }
