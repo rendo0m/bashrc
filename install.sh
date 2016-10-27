535a1c3efdef54594b4e77f3f04976927d06f184
rendo0m <andi.arbait@gmail.com>
2016-10-28 01:31:13 +0200

    install script corrected

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-ln -s .bashrc.d/bashrc .bashrc
-ln -s .bashrc.d/bash_profile .bash_profile
-ln -s .bashrc.d/profile .profile
-ln -s .bashrc.d/weechat/ .weechat
-ln -s .bashrc.d/tmux.conf .tmux.conf
-ln -s .bashrc.d/vim .vim
-ln -s .bashrc.d/minttyrc .minttyrc
-ln -s .bashrc.d/ctags .ctags
-ln -s .bashrc.d/dircolors .dircolors
+
+rm -rf .bashrc 2> /dev/null && ln -s .bashrc.d/bashrc .bashrc
+rm -rf .bash_profile 2> /dev/null && ln -s .bashrc.d/bash_profile .bash_profile
+rm -rf .profile 2> /dev/null && ln -s .bashrc.d/profile .profile
+rm -rf .weechat 2> /dev/null && ln -s .bashrc.d/weechat/ .weechat
+rm -rf .tmux.conf 2> /dev/null && ln -s .bashrc.d/tmux.conf .tmux.conf
+rm -rf .vim 2> /dev/null && ln -s .bashrc.d/vim .vim
+rm -rf .minttyrc 2> /dev/null && ln -s .bashrc.d/minttyrc .minttyrc
+rm -rf .ctags 2> /dev/null && ln -s .bashrc.d/ctags .ctags
+rm -rf .dircolors 2> /dev/null && ln -s .bashrc.d/dircolors .dircolors
