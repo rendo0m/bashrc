8205185d5fb75c899b28ac9a876653f2f313b68d
rendo0m <andi.arbait@gmail.com>
2016-11-04 18:02:03 +0100

    litte slash correcting

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 rm -rf .bashrc 2> /dev/null && ln -s .bashrc.d/bashrc .bashrc
 rm -rf .bash_profile 2> /dev/null && ln -s .bashrc.d/bash_profile .bash_profile
 rm -rf .profile 2> /dev/null && ln -s .bashrc.d/profile .profile
-rm -rf .weechat 2> /dev/null && ln -s .bashrc.d/weechat/ .weechat
+rm -rf .weechat 2> /dev/null && ln -s .bashrc.d/weechat .weechat
 rm -rf .tmux.conf 2> /dev/null && ln -s .bashrc.d/tmux.conf .tmux.conf
 rm -rf .vim 2> /dev/null && ln -s .bashrc.d/vim .vim
 rm -rf .minttyrc 2> /dev/null && ln -s .bashrc.d/minttyrc .minttyrc
