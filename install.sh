e70261dc9ed82e7f79f263ed8634a4618a49e16e
rendo0m <andi.arbait@gmail.com>
2017-11-03 19:50:22 +0100

    sudo apt update

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ rm -rf .bashrc 2> /dev/null && ln -s .bashrc.d/bashrc .bashrc
 rm -rf .bash_profile 2> /dev/null && ln -s .bashrc.d/bash_profile .bash_profile
 rm -rf .profile 2> /dev/null && ln -s .bashrc.d/profile .profile
 rm -rf .weechat 2> /dev/null && ln -s .bashrc.d/weechat .weechat
-rm -rf .tmux.conf 2> /dev/null && ln -s .bashrc.d/tmux.conf .tmux.conf
-rm -rf .vim 2> /dev/null && ln -s .bashrc.d/vim .vim
+ff -rf .tmux.conf 2> /dev/null && ln -s .bashrc.d/tmux.conf .tmux.conf
+rm -rf .vim 2> /dev/null && rm -rf .vimrc 2> /dev/null && ln -s .bashrc.d/vim/vimrc.local .vimrc
 rm -rf .minttyrc 2> /dev/null && ln -s .bashrc.d/minttyrc .minttyrc
 rm -rf .ctags 2> /dev/null && ln -s .bashrc.d/ctags .ctags
 rm -rf .dircolors 2> /dev/null && ln -s .bashrc.d/dircolors .dircolors
