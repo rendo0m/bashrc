3d4b4d95f8faff97480bb242939dcbeb9c976ae9
rendo0m <andi.arbait@gmail.com>
2021-09-10 14:11:16 +0200

    added nvm to node

--- topics/node.sh
+++ topics/node.sh
@@ -1,4 +1,6 @@
 # Add npm bin to path.
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 if command -v npm &> /dev/null; then
   source <(npm completion 2>&-)
 fi
