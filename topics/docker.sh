1d7d4b960e263d07a8af4248b66a29030391546c
rendo0m <andi.arbait@gmail.com>
2022-11-17 13:41:20 +0100

    check if root

--- topics/docker.sh
+++ topics/docker.sh
@@ -2,6 +2,6 @@
 #make-completion-wrapper _docker _docker_alias
 #complete -F _docker_alias docker
 
-alias dockervol='sudo su root -c "cd /var/lib/docker/volumes; /bin/bash"'
+alias dockervol='if [ ${EUID} -eq 0 ]; then cd /var/lib/docker/volumes; else sudo su root -c "cd /var/lib/docker/volumes; /bin/bash"; fi'
 make-completion-wrapper _docker _dockervol
 complete -F _dockeer_vol dockervol
